8e7c347c2151c7ebba6135e22c3b0618d18a5892
Angel Auñón <angel.aunon.garcia@gmail.com>
2019-04-11 18:44:55 +0200

    Fixed bug in migrate comand

--- app/__init__.py
+++ app/__init__.py
@@ -49,7 +49,7 @@ def create_app(config_class=Config):
         if app.config['ELASTICSEARCH_URL'] else None
 
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
