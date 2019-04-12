b48a4f885298bd1b46ebdd3b374ff7b915b55a13
Angel Auñón <angel.aunon.garcia@gmail.com>
2019-04-12 10:09:30 +0200

    fixed auth/forms.py
    Missing User import

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
+from app.models import User
 
 
 class LoginForm(FlaskForm):
